11988907be3235668ed1d78f752571f8360beffb
Hessu90 <at.posti@gmail.com>
2021-10-30 13:30:35 +0300

    Added Readme and sample config file

---
+++ Tuotokset/juuzo-and-the-haslab-boyz/hessus-code/config/config-sample.js
@@ -0,0 +1,11 @@
+const port = 'port-to-ssh'
+const username = 'username'
+const hostaddress = 'router-ip'
+const serti = 'path-to-cert-file'
+
+module.exports = {
+    port,
+    username,
+    hostaddress,
+    serti
+}
